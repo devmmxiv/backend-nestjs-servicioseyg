c2e80e20fe7949a6b987771d4b37705eca346868
odef <devmmxxiv@gmail.com>
2024-12-16 17:12:35 -0600

    se agregan variables de entornoproduccion  en datasource

--- src/configuration/data-source.ts
+++ src/configuration/data-source.ts
@@ -7,15 +7,15 @@ ConfigModule.forRoot({
     isGlobal:true
   });
   const configService=new ConfigService();
-
+console.log()
   export const DataSourceConfig:DataSourceOptions={
     
     type:'mysql',
-    username:configService.get('DB_USER'),
-    host:configService.get('DB_HOST'),
-    port:configService.get('DB_PORT'),
-    password:configService.get('DB_PASSWORD'),
-    database:configService.get('DB_NAME'),
+    username:process.env.DB_USER || configService.get('DB_USER'),
+    host:process.env.DB_HOST || configService.get('DB_HOST'),
+    port:Number(process.env.DB_PORT) || configService.get('DB_PORT'),
+    password:process.env.DB_PASSWORD ||configService.get('DB_PASSWORD'),
+    database:process.env.DB_NAME ||configService.get('DB_NAME'),
     entities:[__dirname+'/../**/**/*.entity{.ts,.js}'],
     migrations:[__dirname+'/../../migrations/*{.ts,.js}'],
     synchronize:true,
